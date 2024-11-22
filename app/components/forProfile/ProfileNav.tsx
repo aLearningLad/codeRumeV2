654aec942fb5f8418a255aff2e61f221c6e1b0e8
aLearningLad <mocmanca@gmail.com>
2024-11-22 09:40:12 +0200

    dialog additions for profile nav menu

--- app/components/forProfile/ProfileNav.tsx
+++ app/components/forProfile/ProfileNav.tsx
@@ -2,6 +2,14 @@
 
 import { profileNavTabOptions } from "@/lib/enums";
 import { useState } from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 
 const ProfileNav = () => {
   const [isTabSelected, setIsTabSelected] = useState<string>(
@@ -10,6 +18,7 @@ const ProfileNav = () => {
 
   return (
     <section className=" w-full flex flex-col items-start justify-center ">
+      {/* profile */}
       <button
         onClick={() => setIsTabSelected(profileNavTabOptions.PR)}
         className={` w-full h-12 ${
@@ -20,21 +29,44 @@ const ProfileNav = () => {
       >
         Profile
       </button>
-      <button
-        className={` w-full my-5 h-12 bg-slate-950/30 text-white rounded-md flex items-center justify-center text-[14px] `}
-      >
-        Advanced Features
-      </button>
-      <button
-        onClick={() => setIsTabSelected(profileNavTabOptions.AB)}
-        className={` w-full h-12 ${
-          isTabSelected === profileNavTabOptions.AB
-            ? " bg-neutral-100/10 text-white"
-            : "bg-slate-950/30 text-white"
-        } rounded-md flex items-center justify-center text-[14px] `}
-      >
-        About
-      </button>
+
+      {/* advanced features */}
+      <Dialog>
+        <DialogTrigger
+          className={` w-full my-5 h-12 bg-slate-950/30 text-white rounded-md flex items-center justify-center text-[14px] `}
+        >
+          <button>Advanced Features</button>
+        </DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Are you absolutely sure?</DialogTitle>
+            <DialogDescription>
+              This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers.
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+
+      {/* about */}
+      <Dialog>
+        <DialogTrigger
+          className={` w-full h-12 bg-slate-950/30 text-white rounded-md flex items-center justify-center text-[14px] `}
+        >
+          <button onClick={() => setIsTabSelected(profileNavTabOptions.AB)}>
+            About
+          </button>
+        </DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Are you absolutely sure?</DialogTitle>
+            <DialogDescription>
+              This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers.
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
     </section>
   );
 };
