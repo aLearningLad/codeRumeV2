7f19d902eb68fd9dcdfdd284ed51dcf79057ec54
aLearningLad <mocmanca@gmail.com>
2024-11-24 12:36:09 +0200

    progress with types

--- miscdata/newsquares.tsx
+++ miscdata/newsquares.tsx
@@ -1,1 +1,12 @@
+// 1. open a room
+// 2. prepare collaborator list
+// 3. learning resources list (tuts: codecademy, freecodecamp, Ruby, SQL, typescript, C#, C++, java, python)
+// 4. more ---> about this app,
+//         ---> about developer(link to portfolio),
+//         ---> github code,
+//         ---> neon DB,
+//         ---> vercel,
+//         ---> react,
+//         ---> npm,
+
 export const newsquares: Tnewsquares[] = [];
