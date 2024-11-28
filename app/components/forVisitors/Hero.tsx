3e270df9c60106a339cabbb5f6f137d5fb812417
aLearningLad <mocmanca@gmail.com>
2024-11-28 20:29:26 +0200

    more additions, nearing app completion

--- app/components/forVisitors/Hero.tsx
+++ app/components/forVisitors/Hero.tsx
@@ -1,5 +1,13 @@
 import Link from "next/link";
 import HeroDynamic from "./HeroDynamic";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 
 const Hero = () => {
   return (
@@ -15,12 +23,40 @@ const Hero = () => {
       <p className=" mt-5 mb-2 text-lg">Join the codeRume revolution.</p>
       <section className="flex xl:flex-row w-full lg:w-8/12 xl:w-4/12 flex-col items-center gap-2 mt-3 md:mt-5">
         <HeroDynamic />
-        <Link
-          className="w-full xl:w-1/2 hover:bg-cyan-500 transition duration-300 ease-in hover:border-transparent py-5 flex justify-center items-center text-xl border-[1px] border-black text-black rounded-lg"
-          href="/signin"
-        >
-          See how it works
-        </Link>
+        <Dialog>
+          <DialogTrigger className="w-full xl:w-1/2 hover:bg-cyan-500 transition duration-300 ease-in hover:border-transparent py-5 flex justify-center items-center text-xl border-[1px] border-black text-black rounded-lg">
+            <div>See how it works</div>
+          </DialogTrigger>
+          <DialogContent className=" p-5 lg:p-12 min-h-[80vh] lg:min-h-[60vh] flex justify-center items-center flex-col  ">
+            <DialogHeader>
+              <DialogTitle className=" w-full flex  lg:text-xl text-4xl sm:text-3xl md:text-2xl justify-center items-center text-center mb-2">
+                About this application
+              </DialogTitle>
+              <DialogDescription className=" text-black text-lg lg:text-[14px] ">
+                <p>
+                  codeRume by HillSawft&copy; is a fun project that I built to
+                  better understand how to integrate live collaboration
+                  functionalities.
+                </p>
+                <br />
+                <p>
+                  Although I have a fundemental grasp of websockets, this
+                  project provided a new challenge due to the use of NextJS and
+                  it's serverless architecture. In deciding to forgo the need
+                  for a separate Express server, and not being keen to deploy
+                  outside of Vercel, an alternative was found: Enter{" "}
+                  <b>PusherJS</b>!
+                </p>
+                <br />
+                <p>
+                  And so with many re-reads of PusherJS documentation, a lot of
+                  Googling, and temporarily giving up multiple times, the
+                  functionality came together after much trial and error.
+                </p>
+              </DialogDescription>
+            </DialogHeader>
+          </DialogContent>
+        </Dialog>
       </section>
     </div>
   );
--- app/components/forVisitors/VisitorLanding.tsx
+++ app/components/forVisitors/VisitorLanding.tsx
@@ -1,10 +1,3 @@
-// "use client";
-
-// import { OAuthStrategy } from "@clerk/types";
-// import { SignUp, useSignIn, useSignUp } from "@clerk/nextjs";
-// import { useState } from "react";
-// import { isNewOptions } from "@/lib/enums";
-// import Navbar from "../forVisitorLayout/Navbar";
 import Hero from "./Hero";
 import Endorsements from "./Endorsement";
 import UseCases from "./UseCases";
@@ -12,70 +5,8 @@ import Unique from "./Unique";
 import ScalePromo from "./ScalePromo";
 
 const VisitorLanding = () => {
-  // const [isNew, setIsNew] = useState<string>(isNewOptions.N);
-
-  // const { signIn } = useSignIn();
-  // const { signUp } = useSignUp();
-
-  // if (!signIn) return null;
-
-  // // this is for new users
-  // const signUpWith = (strategy: OAuthStrategy) => {
-  //   return signUp?.authenticateWithRedirect({
-  //     strategy,
-  //     redirectUrl: "https://cuddly-parakeet-97.accounts.dev/sign-up",
-  //     redirectUrlComplete: "/profile",
-  //   });
-  // };
-
-  // // this is for users with an exisitng account
-  // const signInWith = (strategy: OAuthStrategy) => {
-  //   return signIn.authenticateWithRedirect({
-  //     strategy,
-  //     redirectUrl: "https://cuddly-parakeet-97.accounts.dev/sign-in",
-  //     //   https://cuddly-parakeet-97.accounts.dev/sign-in
-  //     redirectUrlComplete: "/profile",
-  //   });
-  // };
-
   return (
     <div className="flex w-full px-2 md:px-5 lg:px-10 xl:px-24 flex-col">
-      {/* This is the visitor landing page
-      {(isNew === isNewOptions.N && (
-        <div className="w-full h-full flex justify-center items-center flex-col gap-5">
-          <button
-            className=" w-3/12 h-16 bg-black text-white flex justify-center items-center"
-            onClick={() => signInWith("oauth_google")}
-          >
-            Sign in with Google
-          </button>
-          <button
-            onClick={(e: React.MouseEvent<HTMLButtonElement>) =>
-              setIsNew(isNewOptions.Y)
-            }
-          >
-            I'm new here!
-          </button>
-        </div>
-      )) ||
-        (isNew === isNewOptions.Y && (
-          <div className=" w-full h-full flex flex-col items-center justify-center gap-5">
-            Let's get you signed up!
-            <button
-              onClick={() => signUpWith("oauth_google")}
-              className=" h-16 w-3/12 bg-slate-950 text-white flex justify-center items-center text-center"
-            >
-              Yup, sign me up!
-            </button>
-            <button
-              onClick={(e: React.MouseEvent<HTMLButtonElement>) =>
-                setIsNew(isNewOptions.N)
-              }
-            >
-              Nah, I know these parts. Let me sign in!
-            </button>
-          </div>
-        ))} */}
       <Hero />
       <Endorsements />
       <UseCases />
