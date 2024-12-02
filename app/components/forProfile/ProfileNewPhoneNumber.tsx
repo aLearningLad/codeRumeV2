0663fcf371a0d430c53d183ab5ea4dfa7438cf93
aLearningLad <mocmanca@gmail.com>
2024-12-02 21:55:06 +0200

    phone number fetch call

--- app/components/forProfile/ProfileNewPhoneNumber.tsx
+++ app/components/forProfile/ProfileNewPhoneNumber.tsx
@@ -2,23 +2,58 @@
 
 import { ChangeEvent, useState } from "react";
 import { FaPlus } from "react-icons/fa";
+import { IoCloudDone } from "react-icons/io5";
+import { ImCross } from "react-icons/im";
 
 const ProfileNewPhoneNumber = () => {
   const [isInputShown, setIsInputShown] = useState<boolean>(false);
   const [phoneNumber, setPhoneNumber] = useState<string>();
 
+  const handleSaveNumber = async () => {
+    try {
+      const result = await fetch("/api/addPhoneNumber", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ phoneNumber }),
+      });
+    } catch (error) {
+      console.log("Error saving new number: ", error);
+    }
+  };
+
   return (
     <div className=" w-full flex flex-col gap-2 items-start">
       {isInputShown ? (
-        <div className=" flex flex-col items-center gap-2 text-center ">
-          <label htmlFor="">Enter your phone number</label>
-          <input
-            type="text"
-            value={phoneNumber}
-            onChange={(e: ChangeEvent<HTMLInputElement>) =>
-              setPhoneNumber(e.target.value)
-            }
-          />
+        <div className=" flex flex-col items-start gap-2 text-start ">
+          <label className="" htmlFor="">
+            Enter your phone number
+          </label>
+          <span className=" flex gap-2 items-center">
+            <input
+              type="text"
+              value={phoneNumber}
+              onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                setPhoneNumber(e.target.value)
+              }
+              className=" h-12 lg:h-10 rounded-md outline-none focus:scale-95 transition-all duration-300 ease-in text-black placeholder:text-neutral-600 px-2 text-[14px] "
+              placeholder="Eg. 012 367 8901"
+            />
+            <button
+              onClick={handleSaveNumber}
+              className=" w-fit px-2 h-full bg-white rounded-md"
+            >
+              <IoCloudDone size={26} className=" text-green-600" />
+            </button>
+
+            <button
+              onClick={() => setIsInputShown(false)}
+              className="w-fit px-2 h-full bg-white rounded-md "
+            >
+              <ImCross size={22} color="red" />
+            </button>
+          </span>
         </div>
       ) : (
         <>
